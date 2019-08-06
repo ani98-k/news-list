aecebeb6b394c34862d753c06d570c2524443f68
Ani <anikazaryan1899@gmail.com>
2019-08-06 15:50:08 +0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChannelsField from './ChannelsField'
+import Button from '../containers/Button'
+import TopNews from '../containers/TopNews'
+const App = () => (
+    <div>
+    <ChannelsField />
+    <Button />
+    <TopNews />
+    </div>
+)
+
+export default App;
